87a5336ed62622e24b4f561e82f819ddf9a81d61
kaij-java <1102368253@qq.com>
2020-08-16 23:49:47 +0800

    添加rocketmq

---
+++ src/main/java/com/xncoding/rocketmq/controller/RestApi.java
@@ -0,0 +1,39 @@
+package com.xncoding.rocketmq.controller;
+
+import com.xncoding.rocketmq.config.JmsConfig;
+import com.xncoding.rocketmq.producer.Producer;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@RestController
+public class RestApi {
+
+    @Autowired
+    private Producer producer;
+
+    private List<String> mesList = Arrays.asList("小小", "爸爸", "妈妈", "爷爷", "奶奶");
+
+
+    @GetMapping("/text/rocketmq")
+    public Object callback() throws Exception {
+        //总共发送五次消息
+        for (String s : mesList) {
+            //创建生产信息
+            Message message = new Message(JmsConfig.TOPIC, "testtag", ("小小一家人的称谓:" + s).getBytes());
+            //发送
+            SendResult sendResult = producer.getProducer().send(message);
+            log.info("输出生产者信息={}",sendResult);
+        }
+        return "成功";
+    }
+}
